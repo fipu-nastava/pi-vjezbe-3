8275c5561581bcabe36b9f7519d4d56f7452e58c
Nikola Tankovic <nikola.tankovic@gmail.com>
2016-10-07 12:31:22 +0200

    Ispravak Pravokutnika

---
+++ Pravokutnik/Pravokutnik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Pravokutnik
+{
+	public class Pravokutnik
+	{
+		// varijable
+		double sirina;
+		double visina;
+
+		// prvi "default" konstruktor
+		public Pravokutnik()
+		{
+		}
+
+		// drugi konstruktor koji prima parametre
+		public Pravokutnik(int a, int b)
+		{
+			this.sirina = a;
+			this.visina = b;
+		}
+
+		public void PostaviVrijednosti(double a, double b)
+		{
+			this.sirina = a;
+			this.visina = b;
+		}
+
+		public double IzracunajPovrsinu()
+		{
+			return sirina * visina;
+		}
+
+		public void IspisiRezultate()
+		{
+			Console.WriteLine("Sirina: {0}", sirina);
+			Console.WriteLine("Visina: {0}", visina);
+			Console.WriteLine("Povrsina: {0}", IzracunajPovrsinu());
+		}
+	}
+}
--- Pravokutnik/Program.cs
+++ Pravokutnik/Program.cs
@@ -1,32 +1,7 @@
 ﻿using System;
 
-namespace PrimjerSintakse
+namespace Pravokutnik
 {
-	class Pravokutnik
-	{
-		// varijable
-		double sirina;
-		double visina;
-
-		public void PostaviVrijednosti(double a, double b)
-		{
-			sirina = a;
-			visina = b;
-		}
-
-		public double IzracunajPovrsinu()
-		{
-			return sirina * visina;
-		}
-
-		public void IspisiRezultate()
-		{
-			Console.WriteLine("Sirina: {0}", sirina);
-			Console.WriteLine("Visina: {0}", visina);
-			Console.WriteLine("Povrsina: {0}", IzracunajPovrsinu());
-		}
-	}
-
 	class GlavniProgram
 	{
 		static void Main(string[] args)
@@ -34,6 +9,11 @@ namespace PrimjerSintakse
 			Pravokutnik r = new Pravokutnik();
 			r.PostaviVrijednosti(4, 5);
 			r.IspisiRezultate();
+
+			//moze i koristeci konstruktor
+
+			Pravokutnik r1 = new Pravokutnik(4, 5);
+			r1.IspisiRezultate();
 		}
 	}
 }
